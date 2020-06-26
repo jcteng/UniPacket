93394d1e991c8912628d725f926ce242a200b040
jianchao.teng <jianchao.teng@outlook.com>
2020-06-27 03:19:33 +0800

    init code

---
+++ UniPacketBG/TaskEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Core;
+using System.Diagnostics;
+namespace UniPacketBG
+{
+    public sealed class TaskEntry:IBackgroundTask
+    {
+        const String UniPackPropPluginKey = "UniPackPropPluginKey";
+        //We don't need async call there yet
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //make this plugin unqiue 
+            object plugin;
+            if (!CoreApplication.Properties.TryGetValue(UniPackPropPluginKey, out plugin))
+            {
+                plugin = new UniPacketPlugin();
+                CoreApplication.Properties[UniPackPropPluginKey] = plugin;
+                Debug.WriteLine("BG run with new Plugin");
+            }
+            else {
+                Debug.WriteLine("BG run with existed Plugin");
+            }
+                        
+            Windows.Networking.Vpn.VpnChannel.ProcessEventAsync(plugin, taskInstance.TriggerDetails);
+
+        }
+    }
+}
---
+++ UniPacketBG/UniPacketPlugin.cs
@@ -0,0 +1,57 @@
+﻿using Windows.Networking.Vpn;
+using System.Diagnostics;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+using System;
+
+namespace UniPacketBG
+{
+    class UniPacketPlugin : IVpnPlugIn
+    {
+        public void Connect(VpnChannel channel)
+        {
+            Debug.WriteLine("VPN run ++");
+            try
+            {
+                var tunnelSocket = new DatagramSocket();
+                tunnelSocket.ConnectAsync(new HostName("127.0.0.1"), "23123").AsTask().Wait();
+                channel.AssociateTransport(tunnelSocket, null);
+                channel.Start(
+                        new[] { new HostName("66.32.12.12") }, //此处可从服务端获取ip配置 或者直接通过dhcp来设置
+                        null,
+                        null,
+                        new VpnRouteAssignment() { ExcludeLocalSubnets = true },
+                        new VpnNamespaceAssignment() { },
+                        512,
+                        1024,
+                        false,
+                        tunnelSocket,
+                        null
+                    );
+            }
+            catch (System.Exception e) {
+                channel.SetErrorMessage(e.Message);
+            }
+        }
+
+        public void Disconnect(VpnChannel channel)
+        {
+            
+        }
+
+        public void GetKeepAlivePayload(VpnChannel channel, out VpnPacketBuffer keepAlivePacket)
+        {
+            keepAlivePacket = null;
+        }
+
+        public void Encapsulate(VpnChannel channel, VpnPacketBufferList packets, VpnPacketBufferList encapulatedPackets)
+        {
+            
+        }
+
+        public void Decapsulate(VpnChannel channel, VpnPacketBuffer encapBuffer, VpnPacketBufferList decapsulatedPackets, VpnPacketBufferList controlPacketsToSend)
+        {
+            
+        }
+    }
+}
